485ac886ab1f70a532861b5c668b20644e251e2f
Tsai Wei-Che <108356037@nccu.edu.tw>
2021-01-11 01:18:20 +0800

    modified helm chart

--- k3s-files/k3sSetup.sh
+++ k3s-files/k3sSetup.sh
@@ -8,6 +8,8 @@ token=$(sudo cat /var/lib/rancher/k3s/server/node-token)
 ####label edge as worker
 k3s kubectl label nodes jetsonv1-desktop kubernetes.io/role=worker
 
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
 #########this is for worker node
 #curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --token ${vlanip} --server ${vlanip}:6443" sh -s -
 curl -sfL https://get.k3s.io | K3S_URL="https://${vlanip}:6443" K3S_TOKEN=${token}  sh -s - --docker
